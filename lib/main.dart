cb8cc5817af5010acae802f021b214d82ad04372
Tomas Mufume <tomasmufume@gmail.com>
2020-11-13 19:32:34 +0200

    segundo commit

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,58 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.brown,
         title: Text("Indice de Massa Corporal", style: TextStyle(color: Colors.white),),
         centerTitle: true,
+
+        actions: [
+
+          IconButton(icon: Icon(Icons.stream), onPressed: () {})
+        ],
+      ),
+      backgroundColor: Colors.yellowAccent,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(padding: EdgeInsets.all(40)),
+
+            Icon(Icons.stream, size: 100, color: Colors.brown,),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: "Pego (Kg)",
+                labelStyle: TextStyle(color: Colors.grey),
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.brown, fontSize: 25, fontWeight: FontWeight.bold),
+
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: "Altura (cm)",
+                labelStyle: TextStyle(color: Colors.grey),
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.brown, fontSize: 25, fontWeight: FontWeight.bold),
+
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 25, bottom: 40),
+              child: Container(
+                height: 50,
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text("Calcular",
+                    style: TextStyle(color: Colors.yellowAccent, fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
+                  color: Colors.brown,
+                ),
+              ),
+            ),
+            Text("Info",textAlign: TextAlign.center, style: TextStyle(color: Colors.brown, fontSize: 25), )
+          ],
+
+        ),
       ),
     );
   }
